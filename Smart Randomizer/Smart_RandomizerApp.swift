2837398651eab642bb69d9bbef97292a603cfa44
Todd Sifleet <todd.sifleet@gmail.com>
2025-01-05 05:43:06 -0600

    Initial Commit

---
+++ Smart Randomizer/Smart_RandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_RandomizerApp.swift
+//  Smart Randomizer
+//
+//  Created by TODD SIFLEET on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_RandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
